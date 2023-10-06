b22bf656317b8f07d15342abb9da118b0af10b25
Kaoru-kubota-Karura <cosmo.kaoru1211@gmail.com>
2023-10-06 13:29:41 +0800

    ver1006

--- src/main/java/com/example/Basket.java
+++ src/main/java/com/example/Basket.java
@@ -3,19 +3,33 @@ package com.example;
 import java.util.ArrayList;
 
 public class Basket {
+    private static Object removeFruit;
     private int capacity;
     private ArrayList<Fruit> fruits;
 
     public Basket(int capacity) {
         // TODO: Initialize the attributes (and initialize the fruits list)
+        this.capacity = capacity;
     }
 
     public void addFruit(Fruit fruit) {
         // TODO: Add the fruit to the basket IF THERE IS SPACE!
+        double apple;
+        double banana;
+        apple = 0.2;
+        banana = 0.3;
+        if (capacity == 0){
+            // remove the data
+            Basket.remove(fruits);
+        }
+        else{
+            capacity = capacity - 1;
+        }
     }
 
     public void removeFruit(Fruit fruit) {
         // TODO: Remove the fruit from the basket
+        Basket.remove(fruit.());
     }
 
     // TODO: Implement getters, setters, and any other methods you find necessary
--- src/main/java/com/example/Fruit.java
+++ src/main/java/com/example/Fruit.java
@@ -8,7 +8,12 @@ public class Fruit {
 
     public Fruit(String name, String color, String taste, double weight) {
         // TODO: Initialize the attributes
+        this.name = name;
+        this.color = color;
+        this.taste = taste;
+        this.weight = weight;
     }
 
     // TODO: Implement getters, setters, and any other methods you find necessary
+
 }
